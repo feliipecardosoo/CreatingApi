8f96b6a788f375569e232845b2f6013ed67f224e
feliipecardosoo <fglibino@gmail.com>
2023-12-11 19:53:49 -0300

    Criando rota POST

--- index.js
+++ index.js
@@ -10,6 +10,15 @@ app.use(
 app.use(express.json())
 
 // rotas - endpoints
+app.post('/createproduct', (req,res) => {
+    const name = req.body.name
+    const price = req.body.price
+
+    console.log(`${name} e o preco é de ${price}`)
+    res.json({message: 'Produto criado com sucesso!'})
+
+})
+
 app.get('/', (req, res) => {
     res.json({message: 'Primeira rota criada com sucesso!'})
 })
